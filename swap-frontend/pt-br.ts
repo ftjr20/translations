0cbe1db262b8eb9e33b08b3f50fb42e2e1520553
ftjr20 <84250399+ftjr20@users.noreply.github.com>
2023-04-04 22:38:46 -0300

    Create pt-br.ts

---
+++ swap-frontend/pt-br.ts
@@ -0,0 +1,48 @@
+/*
+  Traduzido por Fernando Tajiri
+*/
+
+export const TRANSLATIONS_PTBR = {
+    step0_title:"Bem vindo ao SxpSwap!",
+    step0_subtitle: "Esta ferramenta foi desenvolvida para facilitar migração entre as redes SXP(ERC20) e SXP(BEP20) para SXP Mainnet (rede principal). Clique no FAQ abaixo para saber mais sobre esta ferramenta. Caso contrário, clique em próximo para começar a migrar seus tokens para a rede principal.",
+    useful_links: "Links úteis para compreender melhor antes de iniciar a migração.",
+    faq: "Perguntas frequentes",
+    telegram: "Comunidade Solar no Telegram",
+    ready_to_swap: "Pronto para migração? Clique em próximo!",
+    download_wallet: "Antes de iniciar a troca. Você é obrigado a criar uma carteira na rede principal da Solar (SXP). Clique no link abaixo para criar uma carteira",
+    download_wallet_button: "Baixe a carteira Solar da SXP",
+    next: "Próximo",
+    previous: "Anterior",
+    step1_title: "Onde você tem seu SXP?",
+    step1_subtitle: "Selecione o blockchain no qual você mantém seus tokens Swipe (SXP).",
+    synchronizing: "Sincronizando com MetaMask...",
+    install_metamask: "Por favor, instale o Metamask.",
+    metamask_unavailable: "MetaMask não disponível",
+    connecting: "Conectando...",
+    not_connected: "Não conectado",
+    connect_metamask: "Conecte-se à Metamask",
+    step2_title: "Insira o valor SXP.",
+    step2_subtitle: "Insira a quantidade de tokens Swipe (SXP) que deseja trocar. Você não precisa trocar tudo de uma vez na primeira tentativa.",
+    you_have: "Atualmente você tem {{amount}} SXP", // destinado a "você atualmente tem x SXP"
+    swap_all: "migrar tudo", // sempre em minúsculo
+    step3_title: "Qual é o endereço SXP da sua rede principal?",
+    step3_subtitle: "Digite seu endereço de rede principal SXP onde você receberá seus tokens SXP.",
+    enter_address: "Insira um endereço de rede principal SXP válido",
+    not_valid_address: "Este não é um endereço de rede principal SXP válido.",
+    valid_address: "Este é um endereço de rede principal SXP válido.",
+    step4_title: "Troque seu SXP!",
+    step4_subtitle: "Clique no botão aprovar, espere até poder trocar... sim, faça isso!",
+    gonna_swap: "você vai trocar",
+    to: "para",
+    approve_swap: "aprovar troca",
+    approving_swap: "Aprovando troca...",
+    swap_now: "Troque agora",
+    swapping: "Enviando transação de troca...",
+    wait_for_sxp: "Pode levar alguns minutos até que você veja seu SXP em sua Carteira Solar na rede principal.",
+    step5_title: "Você acabou de trocar {{amount}} SXP!",
+    step5_subtitle: "",
+    you_still_have: "Você ainda tem {{balance}} de SXP para trocar. Esperamos que você troque-os em breve!",
+    swap_more: "Trocar mais",
+    see_transaction: "Você pode ver a transação:",
+    click_here: "Clique aqui"
+   };
